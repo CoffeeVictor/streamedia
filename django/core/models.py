2d7a957146f002bf558dc16c46100a8d3817f209
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-10-27 20:35:03 -0300

    Creates tag model

--- django/core/models.py
+++ django/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+	name = models.CharField(max_length=50, unique=True, verbose_name='Nome')
+	class Meta:
+		verbose_name = 'Tag'
+		verbose_name_plural = 'Tags'
+    
+	def __str__(self) -> str:
+		return self.name
