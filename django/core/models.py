09cbb1c50c2c133afd0100cea18c9f447e1c3fe8
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-10-27 20:35:19 -0300

    Creates base Media and Video models

--- django/core/models.py
+++ django/core/models.py
@@ -10,3 +10,26 @@ class Tag(models.Model):
     
 	def __str__(self) -> str:
 		return self.name
+
+class Media(models.Model):
+	class Meta:
+		abstract = True
+
+
+class Video(Media):
+	title = models.CharField(verbose_name='Title', max_length=255)
+	description = models.TextField(verbose_name='Description')
+	thumbnail = models.ImageField(verbose_name='Thumbnail', upload_to='thumbnails/')
+	slug = models.SlugField(unique=True)
+	published_at = models.DateTimeField(verbose_name='Published at', null=True, editable=False)
+	is_published = models.BooleanField(verbose_name='Published', default=False)
+	num_likes = models.IntegerField(verbose_name='Likes', default=0, editable=False)
+	num_views = models.IntegerField(verbose_name='Views', default=0, editable=False)
+	tags = models.ManyToManyField(Tag, verbose_name='Tags', related_name='videos')
+
+	class Meta:
+		verbose_name = 'Video'
+		verbose_name_plural = 'Videos'
+
+	def __str__(self) -> str:
+		return self.title
