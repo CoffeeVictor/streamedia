5d8adc96298ff41c1108d288f456d73f214c0d56
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-10-26 22:37:30 -0300

    Connects django to postgres

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Showcase project for media upload, processing and streaming.
 ## TODO
 
 -   [x] Add docker-compose.yml with database
--   [ ] Add Postgres connection to Django
+-   [x] Add Postgres connection to Django
 -   [ ] Config core app
 -   [ ] Create video media model
 -   [ ] Create Tag system
--- django/streamedia/settings.py
+++ django/streamedia/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'streamedia.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+	"default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "streamedia_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
