91b171f2be24a717a63eb0fbebb09057580b68a2
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-11-01 10:54:06 -0300

    Improves video listing ux

--- django/core/admin.py
+++ django/core/admin.py
@@ -2,14 +2,17 @@ from core.models import Tag, Video
 
 from django.contrib import admin
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, reverse
 from django.urls.resolvers import URLPattern
+from django.utils.html import format_html
 
 
 # Register your models here.
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
+    list_display = ('title', 'published_at', 'is_published',
+                    'num_likes', 'num_views', 'redirect_to_upload', )
 
     def get_urls(self) -> list[URLPattern]:
         base_urls = super().get_urls()
@@ -19,6 +22,12 @@ class VideoAdmin(admin.ModelAdmin):
         ]
 
         return base_urls + custom_urls
+
+    def redirect_to_upload(self, video: Video):
+        url = reverse('admin:core_video_upload', args=[video.id])
+        return format_html(f'<a href="{url}">Upload</a>')
+
+    redirect_to_upload.short_description = 'Upload'
 
     def upload_video(self, request, object_id):
         print('Request:', request)
