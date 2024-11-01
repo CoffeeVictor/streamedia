5727ffc27b77e3f02f08b993bab3b6ea62903a5c
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-11-01 02:18:22 -0300

    Adds custom url for video admin

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,12 +1,26 @@
 from core.models import Tag, Video
 
 from django.contrib import admin
+from django.urls import path
+from django.urls.resolvers import URLPattern
 
 
 # Register your models here.
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
+
+    def get_urls(self) -> list[URLPattern]:
+        base_urls = super().get_urls()
+        custom_urls = [
+            path('<int:id>/video_upload', self.upload_video,
+                 name='core_video_upload')
+        ]
+
+        return base_urls + custom_urls
+
+    def upload_video(self):
+        pass
 
 
 admin.site.register(Tag)
