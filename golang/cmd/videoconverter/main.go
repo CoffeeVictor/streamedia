240da83451b9b197228a0596c80ca8c6e7e38d82
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-11-05 07:25:15 -0300

    Creates number extractor

--- golang/cmd/videoconverter/main.go
+++ golang/cmd/videoconverter/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"regexp"
+	"strconv"
+)
+
 func main() {
 	println("Hello World!")
+}
+
+func extractNumber(fileName string) int {
+	re := regexp.MustCompile(`\d+`)
+	numStr := re.FindString(filepath.Base(fileName))
+	num, err := strconv.Atoi(numStr)
+	
+	if err != nil {
+		return -1
+	}
+
+	return num
 }
