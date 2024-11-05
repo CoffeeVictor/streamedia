ada35f83a7b632c1aaed6b83d0e3f7119455dfa3
CoffeeVictor <victor.jgfsilva@gmail.com>
2024-11-05 08:09:00 -0300

    Implements video processing function

--- golang/internal/converter/task.go
+++ golang/internal/converter/task.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"sort"
@@ -26,6 +27,50 @@ func (vc *VideoConverter) Handle(msg []byte) {
 	if err != nil {
 		vc.LogError(task, "Failed to unmarshall task", err)
 	}
+}
+
+func (vc *VideoConverter) ProcessVideo(task *VideoTask) error {
+	mergedFilePath := filepath.Join(task.Path, "merged.mp4")
+	mpegDashPath := filepath.Join(task.Path, "mpeg-dash")
+
+	slog.Info("Merging chunks", slog.String("path", task.Path))
+
+	err := vc.mergeChunks(task.Path, mergedFilePath)
+
+	if err != nil {
+		vc.LogError(*task, "Failed to merge chunks", err)
+		return err
+	}
+
+	slog.Info("Creating mpeg-dash dir", slog.String("path", task.Path))
+	err = os.MkdirAll(mpegDashPath, os.ModePerm)
+
+	if err != nil {
+		vc.LogError(*task, "Failed to create mpeg-dash directory", err)
+		return err
+	}
+
+	slog.Info("Converting video to mpeg-dash", slog.String("path", task.Path))
+	ffmpegCmd := exec.Command("ffmpeg", "-i", mergedFilePath, "-f", "dash", filepath.Join(mpegDashPath, "output.mpd"))
+
+	output, err := ffmpegCmd.CombinedOutput()
+
+	if err != nil {
+		vc.LogError(*task, "Failed to convert video to mpeg-dash, output"+string(output), err)
+		return err
+	}
+
+	slog.Info("Video converted to mpeg-dash", slog.String("path", mpegDashPath))
+
+	slog.Info("Removing merged file", slog.String("path", mergedFilePath))
+	err = os.Remove(mergedFilePath)
+
+	if err != nil {
+		vc.LogError(*task, "Failed to remove merged file", err)
+		return err
+	}
+
+	return nil
 }
 
 func (vc *VideoConverter) LogError(task VideoTask, msg string, err error) {
